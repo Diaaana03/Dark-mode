b74127093695e28e6d88d877f4bc28bbcbcfacaa
Diaaana03 <88292313+Diaaana03@users.noreply.github.com>
2023-08-17 23:24:55 +0100

    moment added

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ toggleBtn.addEventListener("click", () => {
 const articlesData = articles
   .map((article) => {
     const { title, date, length, snippet } = article;
+    const formatDate = moment(date).format("MMMM Do, YYYY");
     return `<article class="post">
         <h2>${title}</h2>
         <div class="post-info">
-          <span>${date}</span>
+          <span>${formatDate}</span>
           <span>${length}</span>
         </div>
         <p>
